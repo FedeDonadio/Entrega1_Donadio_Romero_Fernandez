bbfa2ae46597d44a0101f5129694e4141b09e1d1
FedeDonadio <federicodonadio@hotmail.com>
2022-06-07 20:49:48 -0300

    Me pedia comitear sin cambios

--- GastroBlog/AppBlog/forms.py
+++ GastroBlog/AppBlog/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class VinoFormulario(forms.Form):
     varietal = forms.CharField()
     origen = forms.CharField()
-    fecha = forms.DateField()
+    fecha = forms.DateField(input_formats='%Y')
     temperatura = forms.IntegerField()
